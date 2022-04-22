55c305138ab5d106a3b8d95963623592bdf724f5
VuTran2601 <nguyenvutran2601@gmail.com>
2022-04-22 20:54:08 +0700

    Update VuTran_Bot.java

--- src/main/java/com/example/Vu/VuTran_Bot.java
+++ src/main/java/com/example/Vu/VuTran_Bot.java
@@ -2,8 +2,6 @@ package com.example.Vu;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
