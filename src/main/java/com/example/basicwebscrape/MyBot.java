e36bc3f4f184f4e9d7083646832f1675ac5a6430
NguyenHuuDanh <danh.nguyenphenomenal@hcmut.edu.vn>
2022-04-25 17:47:35 +0700

    Update MyBot.java

--- src/main/java/com/example/basicwebscrape/MyBot.java
+++ src/main/java/com/example/basicwebscrape/MyBot.java
@@ -14,38 +14,25 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.basicwebscrape.football.*;
 
 public class MyBot extends TelegramLongPollingBot {
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .connectTimeout(Duration.ofSeconds(10))
             .build();
-    public String getCurrentWeather(String cityName) {
-        if (cityName != null && !cityName.isBlank()) {
-            try {
-                String urlString = "https://vi.wttr.in/" + URLEncoder.encode(cityName, StandardCharsets.UTF_8) + "?m?T?tqp0";
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(urlString))
-                        .GET()
-                        .build();
-                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                String body = response.body();
-                String result = body.substring(body.indexOf("<pre>") + 5, body.indexOf("</pre>"));
-                result = result.replace("&quot;", "''");
-                System.out.println(urlString);           
-                //PrintStream out = new PrintStream(System.out, true, "UTF-8");
-                //out.println(result);
-                return result;
-            }
-            catch (Exception ex) {
-                System.out.println("Có lỗi xảy ra");
-                return "Có lỗi xảy ra";
-            }
-        }
-        else {
-            return "Vui lòng nhập tên thành phố !";
-        }
-    }
+    
     @Override
     public void onUpdateReceived(Update update) {
         // TODO
@@ -54,18 +41,171 @@ public class MyBot extends TelegramLongPollingBot {
             String command = update.getMessage().getText();
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
-            if(command.equals("/myname")){
-                String msg = getBotUsername();
+            message.setText("Câu lệnh không hợp lệ");
+            if (command.equals("/infobot")) {
+            	String msg = "Đây là con bot nhỏ của một thằng ngông cuồng.";
+            	message.setChatId(update.getMessage().getChatId().toString());
+                message.setText(msg);
+            }
+            else if (command.equals("/start")){
+                String msg = "Gõ / để hiện danh sách các lệnh.";
                 message.setChatId(update.getMessage().getChatId().toString());
                 message.setText(msg);
             }
-            if(command.contains("/weather")){
-                String[] str = command.split(" ");
-                String msg = getCurrentWeather(str[1]);
-                message.setChatId(update.getMessage().getChatId().toString());
-                message.setText(msg);
+            else if (command.equals("/matches")) {
+            	message.setChatId(update.getMessage().getChatId().toString());
+            	message.setText("Chọn giải đấu");
+            	//Buttons
+				InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+				List < List < InlineKeyboardButton >> buttons = new ArrayList<>();
+				List < InlineKeyboardButton > button_list1 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list2 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list3 = new ArrayList<>();
+
+				InlineKeyboardButton england = new InlineKeyboardButton();
+				england.setText("Premier League");
+				england.setCallbackData("England_matches");
+				InlineKeyboardButton spain = new InlineKeyboardButton();
+				spain.setText("Laliga Santander");
+				spain.setCallbackData("Spain_matches");
+				InlineKeyboardButton italy = new InlineKeyboardButton();
+				italy.setText("Serie A");
+				italy.setCallbackData("Italy_matches");
+				InlineKeyboardButton germany = new InlineKeyboardButton();
+				germany.setText("Bundesliga");
+				germany.setCallbackData("Germany_matches");
+				InlineKeyboardButton france = new InlineKeyboardButton();
+				france.setText("Ligue 1");
+				france.setCallbackData("France_matches");
+				InlineKeyboardButton vietnam = new InlineKeyboardButton();
+				vietnam.setText("V-League");
+				vietnam.setCallbackData("Vietnam_matches");
+				
+				button_list1.add(england);
+				button_list1.add(spain);
+				button_list2.add(italy);
+				button_list2.add(germany);
+				button_list3.add(france);
+				button_list3.add(vietnam);
+				
+				buttons.add(button_list1);
+				buttons.add(button_list2);
+				buttons.add(button_list3);
+				markupInline.setKeyboard(buttons);
+				message.setReplyMarkup(markupInline);
             }
+            else if (command.equals("/standing")) {
+            	message.setChatId(update.getMessage().getChatId().toString());
+            	message.setText("Chọn giải đấu");
+            	//Buttons
+				InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+				List < List < InlineKeyboardButton >> buttons = new ArrayList<>();
+				List < InlineKeyboardButton > button_list1 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list2 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list3 = new ArrayList<>();
+
+				InlineKeyboardButton england = new InlineKeyboardButton();
+				england.setText("Premier League");
+				england.setCallbackData("England_standing");
+				InlineKeyboardButton spain = new InlineKeyboardButton();
+				spain.setText("Laliga Santander");
+				spain.setCallbackData("Spain_standing");
+				InlineKeyboardButton italy = new InlineKeyboardButton();
+				italy.setText("Serie A");
+				italy.setCallbackData("Italy_standing");
+				InlineKeyboardButton germany = new InlineKeyboardButton();
+				germany.setText("Bundesliga");
+				germany.setCallbackData("Germany_standing");
+				InlineKeyboardButton france = new InlineKeyboardButton();
+				france.setText("Ligue 1");
+				france.setCallbackData("France_standing");
+				InlineKeyboardButton vietnam = new InlineKeyboardButton();
+				vietnam.setText("V-League");
+				vietnam.setCallbackData("Vietnam_standing");
+				
+				button_list1.add(england);
+				button_list1.add(spain);
+				button_list2.add(italy);
+				button_list2.add(germany);
+				button_list3.add(france);
+				button_list3.add(vietnam);
+				
+				buttons.add(button_list1);
+				buttons.add(button_list2);
+				buttons.add(button_list3);
+				markupInline.setKeyboard(buttons);
+				message.setReplyMarkup(markupInline);
+            }
+            else if (command.equals("/scorers")) {
+            	message.setChatId(update.getMessage().getChatId().toString());
+            	message.setText("Chọn giải đấu");
+            	//Buttons
+				InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+				List < List < InlineKeyboardButton >> buttons = new ArrayList<>();
+				List < InlineKeyboardButton > button_list1 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list2 = new ArrayList<>();
+				List < InlineKeyboardButton > button_list3 = new ArrayList<>();
+
+				InlineKeyboardButton england = new InlineKeyboardButton();
+				england.setText("Premier League");
+				england.setCallbackData("England_scorers");
+				InlineKeyboardButton spain = new InlineKeyboardButton();
+				spain.setText("Laliga Santander");
+				spain.setCallbackData("Spain_scorers");
+				InlineKeyboardButton italy = new InlineKeyboardButton();
+				italy.setText("Serie A");
+				italy.setCallbackData("Italy_scorers");
+				InlineKeyboardButton germany = new InlineKeyboardButton();
+				germany.setText("Bundesliga");
+				germany.setCallbackData("Germany_scorers");
+				InlineKeyboardButton france = new InlineKeyboardButton();
+				france.setText("Ligue 1");
+				france.setCallbackData("France_scorers");
+				InlineKeyboardButton vietnam = new InlineKeyboardButton();
+				vietnam.setText("V-League");
+				vietnam.setCallbackData("Vietnam_scorers");
+				
+				button_list1.add(england);
+				button_list1.add(spain);
+				button_list2.add(italy);
+				button_list2.add(germany);
+				button_list3.add(france);
+				button_list3.add(vietnam);
+				
+				buttons.add(button_list1);
+				buttons.add(button_list2);
+				buttons.add(button_list3);
+				markupInline.setKeyboard(buttons);
+				message.setReplyMarkup(markupInline);
+            }
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (update.hasCallbackQuery()) {
+        	Message msg = update.getCallbackQuery().getMessage();
+        	CallbackQuery callbackQuery = update.getCallbackQuery();
+        	String data = callbackQuery.getData();
+        	SendMessage message = new SendMessage();
+        	message.setChatId(msg.getChatId().toString());
+        	Standing standing = new Standing();
+        	Matches matches = new Matches();
+        	Scorers scorers = new Scorers();
+        	String league = data.split("_")[0];
+        	String type = data.split("_")[1];
+        	
+        	if (type.equals("standing")) {
+        		message.setText(standing.getMessage(league));
+        	}
+        	if (type.equals("matches")) {
+        		message.setText(matches.getMessage(league));
+        	}
+        	if (type.equals("scorers")) {
+        		message.setText(scorers.getMessage(league));
+        	}
+
             try {
                 execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
@@ -77,12 +217,12 @@ public class MyBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
         // TODO
-        return "InfoBot";
+        return "HuuDanhBot";
     }
 
     @Override
     public String getBotToken() {
         // TODO
-        return "5124630324:AAGd47oSNGFSX2xhEdOmY8-G8BMD0v9rYDc";
+        return "5241098510:AAFv91GSHPZSlMGgvdD3rU3d9rzpwHBadyU";
     }
 }
