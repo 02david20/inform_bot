8111dd005caa3c397328124e036716e040db4408
VuTran2601 <nguyenvutran2601@gmail.com>
2022-04-26 13:56:16 +0700

    Update MyBot.java

--- src/main/java/com/example/basicwebscrape/MyBot.java
+++ src/main/java/com/example/basicwebscrape/MyBot.java
@@ -1,5 +1,6 @@
 package com.example.basicwebscrape;
 
+import com.example.basicwebscrape.OilPrice.OilPrice;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
